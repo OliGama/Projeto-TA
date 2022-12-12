9c5743350dc282eb8e1d2726bfec858dd74c2980
João Rodolpho Vitor Cabral <74922267+JoaoKbral@users.noreply.github.com>
2022-12-12 11:01:06 -0300

    Update config.php
    mudei pra seguir o arduino

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 /* Credenciais do banco de dados. Supondo que você esteja executando o MySQL
 servidor com configuração padrão (usuário 'root' sem senha) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'test');
+define('DB_USERNAME', 'arduino');
+define('DB_PASSWORD', 'arduino');
+define('DB_NAME', 'arduino');
  
 /* Tentativa de conexão com o banco de dados MySQL */
 try{
